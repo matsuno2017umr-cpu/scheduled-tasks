12653fba2eb0273ca95bfb209f69d4e4ae9eea1d
matsuno2017umr-cpu <matsuno2017umr@gmail.com>
2026-03-20 14:55:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,44 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
-import smtplib
 import os
+import requests
+from twilio.rest import Client
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+api_key = os.environ.get("OWM_API_KEY")
+MY_LAT = 3.139003
+MY_LONG = 101.686852
+# api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+# api.openweathermap.org/data/2.5/forecast?lat={3.139003}&lon={101.686852}&appid={f1367254f3e7fc423b0d1179f4220408}
+account_sid = os.environ.get("ACCOUNT_SID")
+auth_token = os.environ.get("AUTH_TOKEN")
+call_number = os.environ.get("CALL_NUMBER")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+parameters = {
+    "lat": MY_LAT,
+    "lon": MY_LONG,
+    "appid": api_key,
+    "cnt": 4
+}
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+response = requests.get("https://api.openweathermap.org/data/2.5/forecast", params=parameters)
+response.raise_for_status()
+data = response.json()
+print(data["list"][0]["weather"])
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+nearest = data["list"][0]["weather"][0]["id"]
+print("次の3時間予報:", nearest)
+
+will_rain = False
+for forecast in data["list"]:
+    # dt_txt = forecast["dt_txt"]  # 例: "2026-03-19 12:00:00"
+    # temp = forecast["main"]["temp"]  # 気温（units=metricなら℃）
+    weather_desc = forecast["weather"][0]["id"]
+    if int(weather_desc) < 700:
+        will_rain = True
+
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body="It's going to rain!",
+        from_="+18143288412",
+        to=call_number
+    )
+    print(message.status)
